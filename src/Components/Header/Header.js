a2229ca2f6a7a4b1543b9170651096a1fb1d7a48
Kateryna <kuzikova8katena@gmail.com>
2022-10-05 22:52:19 +0300

    from a class component Information  to a functional

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 export default function Header (...props) {
     return (
       <React.Fragment>
-        <h1>{this.props.text}</h1>
+        <h1>{props.text}</h1>
       </React.Fragment>
     );
   }
